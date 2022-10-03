48487f5489b9083671a3aa915bd6444b9880af70
Matias98cs <matias98cs@gmail.com>
2022-10-03 14:50:53 -0300

    se agrego el nombre en el modelo

--- models/CoffeeByKilogram.js
+++ models/CoffeeByKilogram.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
+  name: {type: String, require: true},
   photo: {type: String, require: true},
   origin: {type: String, require: true},
   presentation: {type : String, require: true},
