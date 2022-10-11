b11989c8fa80bd745207e5003cba224c52b8b7c0
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-10-11 11:23:39 -0300

    fix order controller

--- controllers/ShopOrderControllers.js
+++ controllers/ShopOrderControllers.js
@@ -2,11 +2,11 @@ const Order = require('../models/ShopOrder')
 
 const shopOrderController = {
     create: async (req, res) => {
-        let {user, products: [item, name, quantity, photo, price, totalPrice], total} = req.body
         try {
-            let payment = false
-            let state = "En proceso"
-            let newOrder = await new Order({user, products: [item, name, quantity, photo, price, totalPrice], total, payment, state}).save()
+            let newOrder = new Order(req.body)
+            newOrder.payment = false
+            newOrder.state = "En proceso"
+            await newOrder.save()
             res.status(201).json({
                 message: 'orden creada',
                 response: newOrder,
@@ -27,7 +27,7 @@ const shopOrderController = {
         }
         try {
             let orders = await Order.find(query)
-            .populate('user', {firtName:1, lastName:1, address:1, city:1, province:1, zipCode:1, email:1, tel:1, dni:1 })
+            .populate('user', { firtName:1, lastName:1, address:1, city:1, province:1, zipCode:1, email:1, tel:1, dni:1 })
 
             if (orders) {
                 res.status(200).json({
