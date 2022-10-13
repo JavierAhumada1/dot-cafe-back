a23d04f9a95e50d4a91de5f7e466eb60f800a781
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-10-13 12:31:10 -0300

    Controller added

--- routes/coffeeMaRouter.js
+++ routes/coffeeMaRouter.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {create, read, readByCamp} = require('../controllers/CoffeeMachineControlers')
+const {create, read, readByCamp, readOne} = require('../controllers/CoffeeMachineControlers')
 
 router.get('/', read)
 router.get('/all', readByCamp)
 router.post('/', create)
+router.get('/:id', readOne)
 
 module.exports = router;
